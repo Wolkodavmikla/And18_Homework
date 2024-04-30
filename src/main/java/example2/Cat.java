765ea1e7d68c8401fafb28930723f8c28e774dc8
Uladzimir <lineage200909@mail.ru>
2024-04-30 10:13:08 +0300

    lesson7 changed

--- src/main/java/example2/Cat.java
+++
@@ -1,7 +0,0 @@
-package example2;
-
-public class Cat implements Voice{
-    public void doVoice() {
-        System.out.println("Мяу-мяу");
-    }
-}
--- src/main/java/example2/Dog.java
+++
@@ -1,8 +0,0 @@
-package example2;
-
-public class Dog implements Voice {
-    public void doVoice(){
-        System.out.println("Гав-гав");
-    }
-
-}
